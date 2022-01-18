7d976be6e58a2462a89821fb326281caa00eae37
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2022-01-18 14:38:04 +0530

    Update Q1_toggle.java

--- CP/Bit_Manipulation/Q1_toggle.java
+++ CP/Bit_Manipulation/Q1_toggle.java
@@ -2,10 +2,10 @@
 // we will use xor operation.
 class Bits {
     public static void main(String[] args) {
-        int n = 7; // 0111
-        int pos = 2; // position at which we have to perform toggle.
-        int bit_mask = 1<<pos; // left shift 0001 by pos = 2 value, i.e. 0100
-        int toggle_output = n^bit_mask; // n xor bit_mask i.e 0111^0100 = 0011
-        System.out.println(toggle_output); // 0011 i.e 3 will be our output.
+        int n = 7;                                   // 0111
+        int pos = 2;                                 // position at which we have to perform toggle.
+        int bit_mask = 1<<pos;                       // left shift 0001 by pos = 2 value, i.e. 0100
+        int toggle_output = n^bit_mask;              // n xor bit_mask i.e 0111^0100 = 0011
+        System.out.println(toggle_output);           // 0011 i.e 3 will be our output.
     }
 }
