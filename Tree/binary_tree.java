a68921479fab2bd39d106471e96132377ce0e4d6
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2022-02-09 22:11:12 +0530

    Update binary_tree.java

--- Tree/binary_tree.java
+++ Tree/binary_tree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BinaryTreeClass {
     
     static class Node{
@@ -61,6 +62,71 @@ public class BinaryTreeClass {
             postorder(root.right);
             System.out.print(root.data + " ");
         }
+        
+        public static void levelorder(Node root){
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            
+            while(!q.isEmpty()){
+                Node currentNode = q.remove();   //current node = 0th index element of queue in ist pass and so on.
+                if(currentNode == null){        // if at any point we encounter null in our queue.
+                System.out.println();  // for new line at every null.
+                    if(q.isEmpty()){            // if null is encounter at the end of the queue we will break the loop here.
+                        break;
+                    } else{
+                        q.add(null);            // if not the last null then simply add another null.
+                    }
+                } 
+                else{
+                    // if current node is not null then just add its right and left children in the queue.
+                    System.out.print(currentNode.data + " ");
+                    if(currentNode.left != null){
+                        q.add(currentNode.left);
+                    }
+                    if(currentNode.right != null){
+                        q.add(currentNode.right);
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        
+        int leftsubtree = countNodes(root.left);
+        int rightsubtree = countNodes(root.right);
+        
+        return leftsubtree + rightsubtree + 1;
+    }
+    
+    public static int sumNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftsubtree = sumNodes(root.left);
+        int rightsubtree = sumNodes(root.right);
+        
+        return leftsubtree + rightsubtree + root.data; 
+    }
+    
+    
+    public static int heightOfTree(Node root){
+        if(root == null){
+            return 0;
+        }
+        
+        int leftHeight = heightOfTree(root.left);
+        int rightHeight = heightOfTree(root.right);
+        
+        
+        int treeHeight = Math.max(leftHeight, rightHeight) + 1;
+        
+        return treeHeight;
     }
     
     
@@ -70,12 +136,21 @@ public class BinaryTreeClass {
       BinaryTree b = new BinaryTree();
       Node root = b.buildtree(nodes);
       
-      
+      System.out.print(countNodes(root));
+        
+      System.out.print(heightOfTree(root));
+        
+      System.out.print(sumNodes(root));
+        
       b.preorder(root);
       System.out.println();
+        
       b.inorder(root);
       System.out.println();
+        
       b.postorder(root);
+        
+      b.levelorder(root);
     }
 }
 
