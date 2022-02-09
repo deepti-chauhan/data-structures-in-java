7400dc57cb6f246618f804fe8d6cfa1ac5634b7a
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2022-02-09 21:55:33 +0530

    Create height_of_tree.java

---
+++ Tree/height_of_tree.java
@@ -0,0 +1,11 @@
+public static int heightOfTree(Node root){
+  if(root == null){
+    return 0;
+  }
+  
+  int leftHeight = heightOfTree(root.left);
+  int rightHeight = heightOfTree(root.right);
+  
+  int treeHeight = Math.max(leftHeight, rightHeight) + 1;
+  return treeHeight;
+}
