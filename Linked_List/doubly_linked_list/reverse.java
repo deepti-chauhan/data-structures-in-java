fd42574f9973477a0be8da305023bb0a796f966a
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2022-04-12 18:18:41 +0530

    Create reverse.java

---
+++ Linked_List/doubly_linked_list/reverse.java
@@ -0,0 +1,18 @@
+/*
+  input   :  1 <-> 2 <-> 3 <-> 4 -> null
+  reverse :  4 <-> 3 <-> 2 <-> 1 -> null 
+*/
+
+public void reverse(){
+  Node temp = null;                 
+  Node curr = head;                 
+  
+  while(curr != null){
+    temp = curr.prev;                
+    curr.prev = curr.next;
+    curr.next = temp.
+    curr = curr.prev;
+  }
+  
+  if(temp != null){  head = temp.prev;  }
+}
